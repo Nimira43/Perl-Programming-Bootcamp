73a922914b8eb7dcbc5bf0e8fc3bce758e6e0fc1
Nimira43 <n.rabone72@aol.com>
2024-09-12 13:04:23 +0100

    Single line comments and multiple line comments.

--- StartProject.pl
+++ StartProject.pl
@@ -1,9 +1,18 @@
 #!/usr/bin/env perl
 
+# pragmas's
 use strict;
 use warnings;
 
+# declaring variable
 my $name = "Leonardo";
+
+=begin
+This is a  
+multiple
+line
+comment
+=cut
 
 print("Hello $name Perl World!\n");
 
